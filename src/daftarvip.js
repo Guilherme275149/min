2c122b80140bc1561d06a66f934f8a969a8f0d9b
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-03-09 09:03:49 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,18 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5511934488829ou digite *${prefix}owner*_
+
+*𝙼𝙸𝙽𝙰𝚃𝙾 𝙱𝙾𝚃:*
+`
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5511934488829*
+║
+╚═〘  𝙼𝙸𝙽𝙰𝚃𝙾 𝙱𝙾𝚃 〙
+`
+}
+exports.iklan = iklan
